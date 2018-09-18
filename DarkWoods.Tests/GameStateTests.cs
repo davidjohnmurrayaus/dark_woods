667b55f9f2ea6d772c9b6837fa790915f8097021
David Murray <davidjohnmurray1@gmail.com>
2018-09-18 13:53:24 +1000

    Adding first unit test.

--- DarkWoods.Tests/GameStateTests.cs
+++ DarkWoods.Tests/GameStateTests.cs
@@ -1,14 +1,24 @@
-﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace DarkWoods.Tests
 {
+    /// <summary>
+    /// Tests for the game state.
+    /// </summary>
     [TestClass]
     public class GameStateTests
     {
+        /// <summary>
+        /// Check that a new game starts in a valid state.
+        /// </summary>
         [TestMethod]
-        public void TestMethod1()
+        public void NewGameTests()
         {
+            var myGame = new Data.GameState();
+
+            Assert.IsTrue(myGame.CurrentPage != null, "A new game must start on a valid page.");
+            Assert.IsFalse(string.IsNullOrWhiteSpace(myGame.CurrentPageName), "The current page must have a name.");
+            Assert.IsTrue(myGame.CurrentPage.Transitions.Count > 0, "The page must have at least on transition.");
         }
     }
 }
